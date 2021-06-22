1a73a085d33cda8e3d73ed46b11c3aa264b80ade
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2021-06-22 21:07:14 +0100

    Setup the TakeDamage function as part of the firing mechanism

--- Source/SimpleShooter/Actors/Gun.cpp
+++ Source/SimpleShooter/Actors/Gun.cpp
@@ -8,6 +8,8 @@
 #include "DrawDebugHelpers.h"
 #include "ToolContextInterfaces.h"
 
+#define OUT
+
 // Sets default values
 AGun::AGun()
 {
@@ -24,24 +26,43 @@ AGun::AGun()
 
 void AGun::PullTrigger()
 {
+	// Play MuzzleFlash
 	UGameplayStatics::SpawnEmitterAttached(MuzzleFlash, Mesh, TEXT("MuzzleFlashSocket"));
 
+	// Setup For line trace
 	APawn* OwnerPawn = Cast<APawn>(GetOwner());
 	AController* OwnerController = OwnerPawn->GetController();
 	if(!OwnerPawn || !OwnerController) return;
 
+	// Get Player Viewpoint (start of line trace)
 	FVector Location;
 	FRotator Rotation;
-	OwnerController->GetPlayerViewPoint(Location, Rotation);
+	OwnerController->GetPlayerViewPoint(OUT Location, OUT Rotation);
 
+	// Calculate end of line trace
 	const FVector End = Location + Rotation.Vector() * MaxRange;
 
+	// Line Trace
 	FHitResult Hit;
-	const bool bSuccess = GetWorld()->LineTraceSingleByChannel(Hit, Location, End, ECC_GameTraceChannel1);
-	if(!bSuccess) return;
+	const bool bSuccess = GetWorld()->LineTraceSingleByChannel(OUT Hit, Location, End, ECC_GameTraceChannel1);
 
-	const FVector ShotDirection = -Rotation.Vector();
-	UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), HitEffect, Hit.Location, ShotDirection.Rotation());
+	// If something gets hit...
+	if(bSuccess)
+	{
+		// Spawn Hit FX pointing back to the line trace start
+		const FVector ShotDirection = -Rotation.Vector();
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), HitEffect, Hit.Location, ShotDirection.Rotation());
+
+		AActor* ActorHit = Hit.GetActor();
+		if(ActorHit != nullptr)
+		{
+			FPointDamageEvent DamageEvent(Damage, Hit, ShotDirection, nullptr);
+			ActorHit->TakeDamage(Damage, DamageEvent, OwnerController, this);
+		}
+	}
+
+
+	
 }
 
 // Called when the game starts or when spawned
